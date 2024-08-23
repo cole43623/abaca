cd4174cff1ff34e08943a52567db6369219237d4
Simone <oksimone.marinelli@gmail.com>
2024-08-23 16:55:07 +0200

    sdfsd

--- check.py
+++ check.py
@@ -2,9 +2,16 @@ from par import tutte, cof
 import random
 
 
-for k in cof:
-	if k not in tutte:
-		print(k)
+for k in range(20):
+	p=input().upper()
+	if len(p)>2 and p not in cof and p in tutte:
+		print('"'+p+'"\n')
+		cof.append(p)
+
+cof.sort()
+
+with open("nuova.txt", "w") as file:
+    file.write("cof = "+str(cof))
 
 
 '''
